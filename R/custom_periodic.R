4aec4fc5147aaadcb3cb5bf7a7ae0e769b10c2c3
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2024-10-17 11:23:40 -0400

    add option to silence log messages

--- R/custom_periodic.R
+++ R/custom_periodic.R
@@ -22,19 +22,21 @@ custom_periodic <- function(target_var, target_agg='mean',
                             period_var, period_agg, 
                             period = 3, circular = FALSE,
                             checkNA = TRUE, stopNA = TRUE,
-                            out_name=NA) {
+                            out_name=NA, verbose=F) {
   
   # Check for same extent, number of rows and columns, projection,
   # resolution, and origin
   sameGeom <- class(purrr::reduce(list(target_var, period_var) |>
                                     purrr::discard(is.null),
                                   bioclima::testGeom))
-  if (sameGeom == "SpatRaster") {
-    message("SpatRasters have same extent, number of rows and columns, ",
-            "projection, resolution, and origin")
+  if (verbose == T) {
+    if (sameGeom == "SpatRaster") {
+      message("SpatRasters have same extent, number of rows and columns, ",
+              "projection, resolution, and origin")
+    }
+    
+    warn("This function does not (yet) check for or handle NA values in inputs. NAs in output may result.")
   }
-  
-  message("Warning: this function does not (yet) check for or handle NA values in inputs. NAs in output may result.")
   
   period_summed <- bioclima::sum_period(period_var, period, circular)
   target_summed <- bioclima::sum_period(target_var, period, circular)
@@ -42,15 +44,12 @@ custom_periodic <- function(target_var, target_agg='mean',
   # if target variable is temperature, return mean of temperature input, not sum
   # for example, minimum temperature (quarterly mean)
   if (target_agg == 'mean') {
-    message("Returning periodic mean of target variable. For precipitation as target variable, 
-            this is incorrect. Instead, set target_agg to 'sum'.")
+    if (verbose == T) {
+      message("Returning periodic mean of target variable. For precipitation as target variable, 
+              this is incorrect. Instead, set target_agg to 'sum'.")
+    }
     target_summed = target_summed / period
   }
-  
-  message(
-    paste0("Custom periodic calculations used a period of ", period,
-           " units with", if(circular == FALSE) "out", " circularity.")
-  )
   
   if (period_agg == 'max') {
     custom_bc <- terra::selectRange(target_summed, terra::which.max(period_summed))
@@ -61,6 +60,13 @@ custom_periodic <- function(target_var, target_agg='mean',
   if (!is.na(out_name)) {
     names(custom_bc) <- out_name
   }
+  
+  if (verbose == T) {
+    message(
+      paste0("Custom periodic calculations used a period of ", period,
+             " units with", if(circular == FALSE) "out", " circularity."))
+  }
+  
   return(custom_bc)
   
 }
